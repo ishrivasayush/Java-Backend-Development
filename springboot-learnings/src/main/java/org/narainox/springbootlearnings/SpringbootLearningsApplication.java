63e918b30bbae0b56fb54bec685bad0e9e64c565
Ayush Shrivastava <ayusrivastava2k3@gmail.com>
2023-12-02 00:30:26 +0530

    Day 2

---
+++ springboot-learnings/src/main/java/org/narainox/springbootlearnings/SpringbootLearningsApplication.java
@@ -0,0 +1,13 @@
+package org.narainox.springbootlearnings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLearningsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootLearningsApplication.class, args);
+	}
+
+}
